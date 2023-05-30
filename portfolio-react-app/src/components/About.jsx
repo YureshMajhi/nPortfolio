31c8ab8486d6906a3a90a3c51a382e48c2f2b4a5
YureshMajhi <yureshmee@gmail.com>
2023-05-31 02:21:59 +0545

    small adjustments

--- portfolio-react-app/src/components/About.jsx
+++ portfolio-react-app/src/components/About.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-[#0a192f] text-gray-300 ">
+    <div
+      name="about"
+      className="w-full h-screen flex flex-col justify-center items-center bg-[#0a192f] text-gray-300 "
+    >
       <div className="w-full max-w-[1000px] px-4 grid grid-cols-2 gap-8">
         <div className="sm:text-right pb-8 pl-4">
           <p className="text-4xl font-bold border-b-4 border-pink-600 inline">
